b319c7f889a4561e11539b84ff70d7854f6d113a
gabby <gehandy123@gmail.com>
2021-11-03 23:01:01 +0000

    Got it to quit

--- calculator.py
+++ calculator.py
@@ -2,6 +2,13 @@
 
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
+while True:
+    answer = input("Enter your equation >>> ")
 
+    tokens = answer.split(" ")
 
+    if tokens[0] == "q":
+        print("You ended the game")
+        quit() 
+    
 # Replace this with your code
